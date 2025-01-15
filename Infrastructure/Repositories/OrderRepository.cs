961070050a6b1c2066fd3878411f26c3b095180d
Navruz Komilov <navruzkomilov503@gmail.com>
2025-01-15 20:52:16 +0500

    add repository

---
+++ Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,22 @@
+﻿
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class OrderRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public OrderRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Order>> Get()
+        {
+            return await _dbContext.Orders
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
