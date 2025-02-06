450863c440516ff43089c3ff127b93a60c28eb98
Navruz Komilov <navruzkomilov503@gmail.com>
2025-02-06 22:13:59 +0500

    Add CookerRepository

---
+++ Infrastructure/Repositories/ICookerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public interface ICookerRepository
+    {
+        Task<int> CreateAsync(Cooker cooker);
+        Task<int> UpdateAsync(int id, int contactId);
+        Task<int> DeleteAsync(int id);
+        Task<Cooker?> GetAsync(int id);
+        Task<List<Cooker>> GetAllAsync();
+    }
+}
