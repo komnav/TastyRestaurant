44520c65a199db78e71c8a83ebe0f8e0c7bb7690
Navruz Komilov <navruzkomilov503@gmail.com>
2025-02-07 21:34:54 +0500

    Add OrderDetailRepository

---
+++ Infrastructure/Repositories/IOrderDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+
+namespace Infrastructure.Repositories
+{
+    public interface IOrderDetailRepository
+    {
+        Task<int> CreateAsync(OrderDetail orderDetail);
+        Task<int> UpdateAsync(int id, int orderId, int menuItemId, int quantity, int price, OrderDetailStatus status);
+        Task<int> DeleteAsync(int id);
+        Task<OrderDetail?> GetAsync(int id);
+        Task<List<OrderDetail>> GetAllAsync();
+    }
+}
---
+++ Infrastructure/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class OrderDetailRepository(ApplicationDbContext dbContext) : IOrderDetailRepository
+    {
+        private readonly ApplicationDbContext _dbContext = dbContext;
+        public async Task<int> CreateAsync(OrderDetail orderDetail)
+        {
+            await _dbContext.OrderDetails.AddAsync(orderDetail);
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            return await _dbContext.OrderDetails
+                .Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+        }
+
+        public async Task<List<OrderDetail>> GetAllAsync()
+        {
+            return await _dbContext.OrderDetails.ToListAsync();
+        }
+
+        public async Task<OrderDetail?> GetAsync(int id)
+        {
+            return await _dbContext.OrderDetails.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<int> UpdateAsync(int id, int orderId, int menuItemId, int quantity, int price, OrderDetailStatus status)
+        {
+            return await _dbContext.OrderDetails
+                .Where(x => x.Id == id)
+                .ExecuteUpdateAsync(x => x
+                .SetProperty(x => x.OrderId, orderId)
+                .SetProperty(x => x.MenuItemId, menuItemId)
+                .SetProperty(x => x.Quantity, quantity)
+                .SetProperty(x => x.Price, price)
+                .SetProperty(x => x.Status, status));
+        }
+    }
+}
