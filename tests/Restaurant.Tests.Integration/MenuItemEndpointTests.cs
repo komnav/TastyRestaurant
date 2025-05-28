2801dd0bc19c250589237b8c0f71dbefb009dbc0
Navruz Komilov <navruzkomilov503@gmail.com>
2025-05-28 11:17:53 +0500

    Create test for CreateMenuItem

---
+++ tests/Restaurant.Tests.Integration/MenuItemEndpointTests.cs
@@ -0,0 +1,29 @@
+using System.Net.Http.Json;
+using Application.Dtos.MenuItem.Requests;
+using Domain.Entities;
+using Domain.Enums;
+using FluentAssertions;
+
+namespace Restaurant.Tests.Integration;
+
+public class MenuItemEndpointTests : BaseTest
+{
+    [Test]
+    public async Task CreateMenuItemByIdEndpointTest()
+    {
+        //Arrange
+        var requestForCreated = new CreateMenuItemRequestModel(1, 50, "Burger5");
+
+        //Act
+        var response = await HttpClient.PostAsJsonAsync("/MenuItem", requestForCreated);
+
+        //Assert
+        response.EnsureSuccessStatusCode();
+        var menuItem = await GetEntity<MenuItem>(m =>
+            m.CategoryId == requestForCreated.CategoryId
+            && m.Price == requestForCreated.Price
+            && m.Name == requestForCreated.Name);
+
+        menuItem.Should().NotBeNull();
+    }
+}
