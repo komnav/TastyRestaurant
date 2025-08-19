6478c9a387b1638fea8ef49caceaf423cdce5c01
Navruz Komilov <navruzkomilov503@gmail.com>
2025-08-19 17:10:11 +0500

    unversioned

---
+++ src/Infrastructure/Migrations/20250726054542_SetNullableToTableId.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SetNullableToTableId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Tables_TableId",
+                table: "Reservations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TableId",
+                table: "Reservations",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Tables_TableId",
+                table: "Reservations",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservations_Tables_TableId",
+                table: "Reservations");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TableId",
+                table: "Reservations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservations_Tables_TableId",
+                table: "Reservations",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ src/Infrastructure/Migrations/20250807090449_Add new property into order.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addnewpropertyintoorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "TableId",
+                table: "Orders",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_TableId",
+                table: "Orders",
+                newName: "IX_Orders_UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_AspNetUsers_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_AspNetUsers_UserId",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Orders",
+                newName: "TableId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                newName: "IX_Orders_TableId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Tables_TableId",
+                table: "Orders",
+                column: "TableId",
+                principalTable: "Tables",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
